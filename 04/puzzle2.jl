42d431f731bde4c5165194dab8a108033ea8efe3
Bohdan Makohin <ajukraine@gmail.com>
2024-12-04 18:25:40 +0200

    day 4: refactor puzzle 2

--- 04/puzzle2.jl
+++ 04/puzzle2.jl
@@ -1,25 +1,22 @@
 using Pipe: @pipe
 using LinearAlgebra
 
-function is_xmas_shape(matrix)
-  shape = ["MAS", "SAM"]
-  join(diag(matrix)) in shape && join(diag(reverse(matrix, dims=2))) in shape
-end
+is_xmas_shape(matrix) = @pipe(
+  [[matrix]; [reverse(matrix, dims=2)]]
+  .|> diag
+  .|> join
+  |> all(in(["MAS", "SAM"]), _)
+)
 
 function solve(filename)
   matrix = stack(collect.(readlines(filename)), dims=1)
   (rows, cols) = size(matrix)
 
-  num_of_shapes = 0
-
-  for i in 1:rows-2, j in 1:cols-2
-    m = @views matrix[i:i+2, j:j+2]
-    if is_xmas_shape(m)
-      num_of_shapes += 1
-    end
-  end
-
-  num_of_shapes
+  @pipe(
+    [@views matrix[i:i+2, j:j+2] for i in 1:rows-2, j in 1:cols-2]
+    |> filter(is_xmas_shape)
+    |> length
+  )
 end
 
-solve("sample.txt") |> println
+solve("input2.txt") |> println
