059281f1c2618b5abf31d4fa62d9f657ea064e02
Bohdan Makohin <ajukraine@gmail.com>
2024-12-08 20:47:34 +0200

    day 8: solve puzzle 2

--- 08/puzzle2.jl
+++ 08/puzzle2.jl
@@ -1,13 +1,42 @@
 # Day 08: Advent of Code 2024
 
-function parse_input(filename)
+using Query, Combinatorics
+
+parse_input(filename) = stack(readlines(filename), dims=1)
+
+function get_antinodes((a, b), grid)
+  dy, dx = Tuple(a - b)
+  d = gcd(dy, dx)
+  ch = CartesianIndex(dy ÷ d, dx ÷ d)
+
+  nodes = Set()
+
+  function fill_nodes(start, incr)
+    while checkbounds(Bool, grid, start)
+      push!(nodes, start)
+      start += incr
+    end
+  end
+
+  fill_nodes(a, ch)
+  fill_nodes(a, -ch)
+
+  nodes
 end
 
 function solve(filename)
-  parse_input(filename)
+  grid = parse_input(filename)
+
+  findall(!isequal('.'), grid) |>
+  @groupby(grid[_]) |>
+  Iterators.flatten ∘ @map(combinations(_, 2)) |>
+  Iterators.flatten ∘ @map(get_antinodes(_, grid)) |>
+  @unique() |>
+  @filter(checkbounds(Bool, grid, _)) |>
+  @count()
 end
 
 solve("input.txt") |> println
 
 # Test with sample
-@assert solve("sample.txt") === nothing
+@assert solve("sample.txt") === 34
