1b649feb17f8aaeea79fe5e6957bfb92c2bffc37
Bohdan Makohin <ajukraine@gmail.com>
2024-12-11 03:30:10 +0200

    day 9: optimize puzzle 2

--- 09/puzzle2.jl
+++ 09/puzzle2.jl
@@ -2,48 +2,52 @@
 _ = nothing # fix 'Missing reference: _' warnings
 
 using Query
+using Curry
 
 parse_input(filename) = parse.(Int, collect(readline(filename)))
 
+struct Block
+  id
+  pos
+  len
+end
+
 const FREE = -1
-is_free((block, segment)) = isequal(block, FREE)
+is_free(block) = isequal(block.id, FREE)
 
 function to_disk_memory(disk_map)
   mem = []
   next = 1
+
+  to_block((i, len)) = Block(iseven(i) ? FREE : i ÷ 2, next, len)
 
   for (i, len) in enumerate(disk_map)
     if len == 0
       continue
     end
 
-    segment = (next, next + len - 1)
+    push!(mem, to_block((i, len)))
     next = next + len
-
-    push!(mem, (iseven(i) ? FREE : i ÷ 2, segment))
   end
 
   mem
+
+  # [(b-a+1,a) for (a,b) in zip(disk_map, cumsum(disk_map))] |>
+  # enumerate |>
 end
 
 function solve(filename)
   mem = parse_input(filename) |> to_disk_memory |> collect
-  map = mem |> @map(fill(_[1], _[2][2] - _[2][1] + 1)) |> Iterators.flatten |> collect
+  map = mem |> @map(fill(_.id, _.len)) |> Iterators.flatten |> collect
 
   free_blocks = mem[findall(is_free, mem)]
   file_blocks = mem[findall(!is_free, mem)]
 
-  could_fit((id, (a1, a2))) = ((r, (b1, b2)),) -> b1 < a1 && (a2 - a1) <= (b2 - b1)
+  could_fit(file) = free -> free.pos < file.pos && file.len <= free.len
 
   function fit(free, file)
-    _, (free_left, free_right) = free
-    file_id, (file_left, file_right) = file
-
-    file_len = file_right - file_left
-    free_len = free_right - free_left
-
-    new_file = (file_id, (free_left, free_left + file_len))
-    new_free = free_len > file_len ? (FREE, (free_left + file_len + 1, free_right)) : nothing
+    new_file = Block(file.id, free.pos, file.len)
+    new_free = free.len > file.len ? Block(FREE, free.pos + file.len, free.len - file.len) : nothing
 
     new_free, new_file
   end
@@ -63,8 +67,8 @@ function solve(filename)
         insert!(free_blocks, free, new_free)
       end
 
-      map[new_file[2][1]:new_file[2][2]] .= new_file[1]
-      map[file_block[2][1]:file_block[2][2]] .= FREE
+      map[range(new_file.pos, length=new_file.len)] .= new_file.id
+      map[range(file_block.pos, length=file_block.len)] .= FREE
     end
   end
 
